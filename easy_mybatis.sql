63562b31638a5a4623321d6db5bae0d9ed387d21
Kaijie234 <abc907558136@qq.com>
2020-03-07 10:40:29 +0800

    update

---
+++ easy_mybatis.sql
@@ -0,0 +1,88 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.5.40 : Database - easy_mybatis
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`easy_mybatis` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `easy_mybatis`;
+
+/*Table structure for table `account` */
+
+DROP TABLE IF EXISTS `account`;
+
+CREATE TABLE `account` (
+  `ID` int(11) NOT NULL COMMENT '编号',
+  `UID` int(11) DEFAULT NULL COMMENT '用户编号',
+  `MONEY` double DEFAULT NULL COMMENT '金额',
+  PRIMARY KEY (`ID`),
+  KEY `FK_Reference_8` (`UID`),
+  CONSTRAINT `FK_Reference_8` FOREIGN KEY (`UID`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `account` */
+
+insert  into `account`(`ID`,`UID`,`MONEY`) values (1,41,100000),(2,45,80000),(3,41,2000);
+
+/*Table structure for table `role` */
+
+DROP TABLE IF EXISTS `role`;
+
+CREATE TABLE `role` (
+  `ID` int(11) NOT NULL COMMENT '编号',
+  `ROLE_NAME` varchar(30) DEFAULT NULL COMMENT '角色名称',
+  `ROLE_DESC` varchar(60) DEFAULT NULL COMMENT '角色描述',
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `role` */
+
+insert  into `role`(`ID`,`ROLE_NAME`,`ROLE_DESC`) values (1,'院长','管理整个学院'),(2,'总裁','管理整个公司'),(3,'校长','管理整个学校');
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(32) NOT NULL COMMENT '用户名称',
+  `birthday` datetime DEFAULT NULL COMMENT '生日',
+  `sex` char(1) DEFAULT NULL COMMENT '性别',
+  `address` varchar(256) DEFAULT NULL COMMENT '地址',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=56 DEFAULT CHARSET=utf8;
+
+/*Data for the table `user` */
+
+insert  into `user`(`id`,`username`,`birthday`,`sex`,`address`) values (41,'kaijie','2020-01-01 17:47:08','男','上海'),(42,'大王','2020-01-03 15:09:37','女','北京'),(43,'小王','2020-02-02 11:34:34','女','天津'),(45,'王炸','2020-02-05 12:04:06','男','广州'),(46,'老王','2020-02-14 17:37:26','女','深圳'),(53,'小芳','2020-03-03 17:57:45','男','杭州');
+
+/*Table structure for table `user_role` */
+
+DROP TABLE IF EXISTS `user_role`;
+
+CREATE TABLE `user_role` (
+  `UID` int(11) NOT NULL COMMENT '用户编号',
+  `RID` int(11) NOT NULL COMMENT '角色编号',
+  PRIMARY KEY (`UID`,`RID`),
+  KEY `FK_Reference_10` (`RID`),
+  CONSTRAINT `FK_Reference_10` FOREIGN KEY (`RID`) REFERENCES `role` (`ID`),
+  CONSTRAINT `FK_Reference_9` FOREIGN KEY (`UID`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `user_role` */
+
+insert  into `user_role`(`UID`,`RID`) values (41,1),(45,1),(41,2);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
